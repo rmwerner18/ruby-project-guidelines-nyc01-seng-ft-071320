fb733d8bc42c74901adbf171a592ce8dd87d4433
humorous-storage-7521 <rmwerner@umich.edu>
2020-07-28 20:40:40 -0400

    changed state code

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ b = ENV["venue_data"]
 venue_api_data = RestClient.get(b)
 venue_info = JSON.parse(venue_api_data)
 
-venue_narrowed_search = venue_info["_embedded"]["venues"].each do |venue_array|
+venues = venue_info["_embedded"]["venues"].each do |venue_array|
     Venue.create(name: venue_array["name"],
                 city: venue_array["city"]["name"],
                 address: venue_array["address"]["line1"])
@@ -29,11 +29,11 @@ a = ENV["data"]
 api_data = RestClient.get(a)
 event_data = JSON.parse(api_data)
 
-narrowed_search = event_data["_embedded"]["events"].select do |event_hash|
-    (event_hash["_embedded"]["venues"][0]["city"]["name"] == "Flushing") 
-end
+# narrowed_search = event_data["_embedded"]["events"].select do |event_hash|
+#     event_hash["_embedded"]["venues"][0]["state"]["name"] == "New York"
+# end
 
-narrowed_search.each do |event_hash|
+events = event_data.each do |event_hash|
     event = Event.create(name: event_hash["name"], 
         date: event_hash["dates"]["start"]["localDate"],
         genre: event_hash["classifications"][0]["genre"]["name"],
@@ -41,15 +41,14 @@ narrowed_search.each do |event_hash|
         venue_id: Venue.find_by(name: event_hash["_embedded"]["venues"][0]["name"]).id)
     end
 
-    # venue_id: ... name: event_hash["_embedded"]["venues"]["name"]).id
 
 # event_by_date = event_data["_embedded"]["events"].select do |event_hash|
 #     event_hash["dates"]["start"]["localDate"] == date
 # end
 
-event_by_state = event_data["_embedded"]["events"].select do |event_hash|
-    event_hash["_embedded"]["venues"][0]["state"]["name"] == "New Jersey"
-end
+# event_by_state = event_data["_embedded"]["events"].select do |event_hash|
+#     event_hash["_embedded"]["venues"][0]["state"]["name"] == "New Jersey"
+# end
 
 # event_by_city = event_data["_embedded"]["events"].select do |event_hash|
 #     event_hash["_embedded"]["venues"][0]["city"]["name"] == "New Jersey"
@@ -65,14 +64,14 @@ end
 # end
 
 
-names_by_state = event_by_state.map do |hash|
-    [
-        hash["name"], 
-        hash["type"],
-        hash["url"],
-        hash["priceRanges"][0]["min"]
-    ]
-end
+# names_by_state = event_by_state.map do |hash|
+#     [
+#         hash["name"], 
+#         hash["type"],
+#         hash["url"],
+#         hash["priceRanges"][0]["min"]
+#     ]
+# end
 
 # binding.pry
 # "something"
